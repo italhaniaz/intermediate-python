c194d1b0c14af69ed70dba817764e784b2a0a2ce
Talha Niaz <italhaniaz@gmail.com>
2026-04-19 06:55:09 +0500

    Implement and practice testing concept

---
+++ practice-code/testing/basicmath.py
@@ -0,0 +1,24 @@
+def div(n1, n2):
+    return n1/n2
+
+
+def sortlist(lst):
+    alist = lst[:]
+    for passnum in range(len(alist)-1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+    return alist
+
+
+import os
+
+def openfile(filename):
+    if not os.path.exists(filename):
+        raise FileNotFoundError
+    
+    open(filename)
+
+
